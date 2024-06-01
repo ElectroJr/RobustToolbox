fd62c6d02f4b65e833f40a04af2059c752130fbe
ElectroJr <leonsfriedrich@gmail.com>
2024-06-01 19:35:55 +1200

    Benchmarks

---
+++ Robust.Benchmarks/IEnumerableBenchmark.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Robust.Shared.Analyzers;
+
+namespace Robust.Benchmarks;
+
+[Virtual]
+[MemoryDiagnoser]
+public class InterfaceBenchmark
+{
+    public HashSet<string> Tags = ["Wall", "Door", "Hat", "Foo", "Bar"];
+    public string[] TagArray = ["Hat", "Bar"];
+    public List<string> TagList = ["Hat", "Bar"];
+
+    [Benchmark(Baseline = true)]
+    public bool HasTagsArray()
+    {
+        return HasTagsArray(TagArray);
+    }
+
+    [Benchmark]
+    public bool HasTagsList()
+    {
+        return HasTagsList(TagList);
+    }
+
+    [Benchmark]
+    public bool HasTagsIEnumerableArray()
+    {
+        return HasTagsIEnumerable(TagArray);
+    }
+
+    [Benchmark]
+    public bool HasTagsIEnumerableList()
+    {
+        return HasTagsIEnumerable(TagList);
+    }
+
+    private bool HasTagsArray(string[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (!Tags.Contains(tag))
+                return false;
+        }
+
+        return true;
+    }
+
+    private bool HasTagsList(List<string> tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (!Tags.Contains(tag))
+                return false;
+        }
+
+        return true;
+    }
+
+    private bool HasTagsIEnumerable(IEnumerable<string> tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (!Tags.Contains(tag))
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Robust.Benchmarks/ParamsBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Robust.Shared.Analyzers;
+
+namespace Robust.Benchmarks;
+
+[MemoryDiagnoser]
+[Virtual]
+public class ParamsBenchmark
+{
+    public HashSet<string> Tags = ["Wall", "Door", "Hat", "Foo", "Bar"];
+    public string Tag = "Hat";
+
+    [Benchmark(Baseline = true)]
+    public bool HasTag()
+    {
+        return HasTag(Tag);
+    }
+
+    [Benchmark]
+    public bool HasTagsParams()
+    {
+        return HasTagsParams(Tag);
+    }
+
+    private bool HasTag(string tag)
+    {
+        return Tags.Contains(tag);
+    }
+
+    private bool HasTagsParams(params string[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (!Tags.Contains(tag))
+                return false;
+        }
+
+        return true;
+    }
+}
